67a95eb54e7314b9e11ad32196d7979dfee1147a
reshmaramesh <ray1191996@gmail.com>
2017-03-10 19:07:47 +0530

    layout done

---
+++ js/main.js
@@ -0,0 +1,282 @@
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alert(jQuery.easing.default);
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+	easeInQuad: function (x, t, b, c, d) {
+		return c*(t/=d)*t + b;
+	},
+	easeOutQuad: function (x, t, b, c, d) {
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeInOutQuad: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t + b;
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInCubic: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t + b;
+	},
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	},
+	easeInOutCubic: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	},
+	easeInQuart: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutQuart: function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	easeInQuint: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	},
+	easeOutQuint: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	},
+	easeInOutQuint: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	},
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	},
+	easeInOutSine: function (x, t, b, c, d) {
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	},
+	easeInExpo: function (x, t, b, c, d) {
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	},
+	easeOutExpo: function (x, t, b, c, d) {
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	},
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	},
+	easeInCirc: function (x, t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	},
+	easeOutCirc: function (x, t, b, c, d) {
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	},
+	easeInOutCirc: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	},
+	easeInElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	easeOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},
+	easeInOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	},
+	easeInBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	easeOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	},
+	easeInOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158; 
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	easeInBounce: function (x, t, b, c, d) {
+		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+	},
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	},
+	easeInOutBounce: function (x, t, b, c, d) {
+		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+});
+/*maps*/
+var myCenter=new google.maps.LatLng(8.6698,77.7390);
+var marker;
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.HYBRID
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  animation:google.maps.Animation.BOUNCE
+  });
+
+marker.setMap(map);
+
+
+infowindow.open(map,marker);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+/*maps-end*/
+/*graphs*/
+var chartData = {
+    "barCircleMobile":[
+        {"index":0.3, "value":17436920, "fill":"#231F20", "label":"WebMd Health"},
+        {"index":0.4, "value":10884799, "fill":"#494551", "label":"Livestrong.com"},
+        {"index":0.5, "value":10257432, "fill":"#564965", "label":"Everyday Health"},
+        {"index":0.6, "value":6110024,  "fill":"#574270", "label":"About.com"},
+        {"index":0.7, "value":3895612,  "fill":"#4D2D77", "label":"Drugs.com"},
+        {"index":0.8, "value":3414585,  "fill":"#684E88", "label":"Alliance Health"},
+        {"index":0.9, "value":3099372,  "fill":"#846F9D", "label":"Lifescript.com"},
+        {"index":1,   "value":2897520,  "fill":"#A494B7", "label":"Health.com"},
+        {"index":1.1, "value":2772679,  "fill":"#CBC3D6", "label":"Health Grades"},
+        {"index":1.2, "value":2636126,  "fill":"#F1EFF4", "label":"Healthline"}
+    ],
+    "barCircleWeb":[
+        {"index":0.3, "value":31588490, "fill":"#231F20", "label":"WebMD Health"},
+        {"index":0.4, "value":26260662, "fill":"#403437", "label":"Everyday Health"},
+        {"index":0.5, "value":24263463, "fill":"#53363C", "label":"Livestrong.com"},
+        {"index":0.6, "value":12795112, "fill":"#5E2C3A", "label":"About.com Health Section"},
+        {"index":0.7, "value":11959167, "fill":"#660E34", "label":"Healthline"},
+        {"index":0.8, "value":10408917, "fill":"#7D3A4D", "label":"HealthGrades"},
+        {"index":0.9, "value":10317462, "fill":"#96606B", "label":"Yahoo! Health"},
+        {"index":1,   "value":9765589,  "fill":"#B28A91", "label":"Lifescript.com"},
+        {"index":1.1, "value":7734964,  "fill":"#D3BCBF", "label":"Health.com"},
+        {"index":1.2, "value":7504000 , "fill":"#EDE4E5", "label":"Drugs.com"}
+    ]
+};
+
+function drawBarCircleChart(data,target,values,labels){
+    var w = 362,
+        h = 362,
+        size = data[0].value * 1.15,
+        radius = 200,
+        sectorWidth = .1,
+        radScale = 25,
+        sectorScale = 1.45,
+        target = d3.select(target),
+        valueText = d3.select(values),
+        labelText = d3.select(labels);
+
+
+    var arc = d3.svg.arc()
+        .innerRadius(function(d,i){return (d.index/sectorScale) * radius + radScale; })
+        .outerRadius(function(d,i){return ((d.index/sectorScale) + (sectorWidth/sectorScale)) * radius + radScale; })
+        .startAngle(Math.PI)
+        .endAngle(function(d) { return Math.PI + (d.value / size) * 2 * Math.PI; });
+    
+    var path = target.selectAll("path")
+        .data(data);
+
+    //TODO: seperate color and index from data object, make it a pain to update object order
+    path.enter().append("svg:path")
+        .attr("fill",function(d,i){return d.fill})
+        .attr("stroke","#D1D3D4")
+        .transition()
+        .ease("elastic")
+        .duration(1000)
+        .delay(function(d,i){return i*100})
+        .attrTween("d", arcTween);
+        
+    valueText.selectAll("tspan").data(data).enter()
+        .append("tspan")
+        .attr({
+            x:50,
+            y:function(d,i){return i*14},       
+            "text-anchor":"end"
+        })
+        .text(function(d,i){return data[i].value});
+    
+    labelText.selectAll("tspan").data(data).enter()
+        .append("tspan")
+        .attr({
+            x:0,
+            y:function(d,i){return i*14}
+        })
+        .text(function(d,i){return data[i].label});
+
+    function arcTween(b) {
+        var i = d3.interpolate({value: 0}, b);
+        return function(t) {
+            return arc(i(t));
+        };
+    }
+}
+
+// Animation Queue
+setTimeout(function(){drawBarCircleChart(chartData.barCircleWeb,"#circleBar-web-chart","#circleBar-web-values","#circleBar-web-labels")},500);
+setTimeout(function(){drawBarCircleChart(chartData.barCircleMobile,"#circleBar-mobile-chart","#circleBar-mobile-values","#circleBar-mobile-labels")},800);
+
+d3.select("#circleBar-web-icon")
+    .transition()
+    .delay(500)
+    .duration(500)
+    .attr("opacity","1");
+d3.select("#circleBar-web-text")
+    .transition()
+    .delay(750)
+    .duration(500)
+    .attr("opacity","1");
+
+d3.select("#circleBar-web-clipLabels")
+    .transition()
+    .delay(600)
+    .duration(1250)
+    .attr("height","150");
+
+d3.select("#circleBar-mobile-icon")
+    .transition()
+    .delay(800)
+    .duration(500)
+    .attr("opacity","1");
+d3.select("#circleBar-mobile-text")
+    .transition()
+    .delay(1050)
+    .duration(500)
+    .attr("opacity","1");
+d3.select("#circleBar-mobile-clipLabels")
+    .transition()
+    .delay(900)
+    .duration(1250)
+    .attr("height","150");
